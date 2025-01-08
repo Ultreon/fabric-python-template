aef8fbc731eb3ef6b63b0d94ae7a02d011015b84
XyperCode <xypercode@ultreon.dev>
2025-01-08 16:37:57 +0100

    Actually trying to improve

--- src/main/python/com/example/client/fabric_client.py
+++
@@ -1,8 +0,0 @@
-from net.fabricmc.api import ClientModInitializer
-from pythonvm.api import implements
-
-
-@implements(ClientModInitializer)
-class ExampleModClient:
-    def onInitializeClient(self):
-        print("Hello Fabric world!")
